bd0ad3f8361bd0831ed99b28ba2dee0ec5f6c60b
PRAGAL <PRAGAL@DESKTOP-CRHJTDS>
2023-02-16 10:42:49 +0400

    projectMavenBit stage changed

---
+++ projectMavenBithub/src/main/java/com/abc/eclipsepgm/mavennew/projectMavenBithub/App.java
@@ -0,0 +1,13 @@
+package com.abc.eclipsepgm.mavennew.projectMavenBithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ projectMavenBithub/src/main/java/com/abc/eclipsepgm/mavennew/projectMavenBithub/SumSimpleExample.java
@@ -0,0 +1,21 @@
+package com.abc.eclipsepgm.mavennew.projectMavenBithub;
+
+public class SumSimpleExample {
+
+	int a=5,b=10;
+	int s;
+	void sum()
+	{
+		s=a+b;
+		System.out.println(s);
+		
+	}
+	public static void main(String[] args) {
+	
+		SumSimpleExample ob=new SumSimpleExample();
+		ob.sum();
+		// TODO Auto-generated method stub
+
+	}
+
+}
